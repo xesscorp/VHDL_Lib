01280a65aa12583cc52208f29973eaa58a1dfcf2
Dave Vandenbout <devb@xess.com>
2015-01-05 10:42:15 -0500

    Added a bit of documentation to the RGB LED driver.

--- RgbLed.vhd
+++ RgbLed.vhd
@@ -21,6 +21,7 @@
 --**********************************************************************
 -- A driver for an RGB LED. It takes the intensity of the red, green and blue
 -- colors and creates PWM outputs to drive the red, green and blue LEDs.
+-- A version with a Wishbone bus interface is also included.
 --**********************************************************************
 
 
@@ -51,15 +52,15 @@ package RgbLedPckg is
       -- Wishbone interface.
       wb_clk_i  : in  std_logic;        -- Input clock.
       wb_rst_i  : in  std_logic;        -- Reset.
-      wb_dat_o  : out std_logic_vector;  -- Data bus from RGB LED driver.
-      wb_dat_i  : in  std_logic_vector;  -- Data bus to RGB LED driver.
-      wb_adr_i  : in  std_logic_vector;  -- Address bus to RGB LED driver.
+      wb_dat_o  : out std_logic_vector; -- Data bus from RGB LED driver.
+      wb_dat_i  : in  std_logic_vector; -- Data bus to RGB LED driver.
+      wb_adr_i  : in  std_logic_vector; -- Address bus to RGB LED driver.
       wb_we_i   : in  std_logic;        -- Write-enable.
       wb_cyc_i  : in  std_logic;        -- Valid cycle indicator.
       wb_stb_i  : in  std_logic;        -- Chip-enable.
-      wb_ack_o  : out std_logic;  -- Acknowledge upon completion of operation.
+      wb_ack_o  : out std_logic;        -- Acknowledge upon completion of operation.
       wb_inta_o : out std_logic;        -- Interrupt from RGB LED driver.
-      id        : out std_logic_vector;  -- Identifier for RGB LED driver.
+      id        : out std_logic_vector; -- Identifier for RGB LED driver.
       -- RGB LED drivers.
       redLed_o  : out std_logic;        -- Red LED PWM output.
       grnLed_o  : out std_logic;        -- Green LED PWM output.
@@ -70,6 +71,11 @@ package RgbLedPckg is
 end package;
 
 
+
+--**********************************************************************
+-- A driver for an RGB LED. It takes the intensity of the red, green and blue
+-- colors and creates PWM outputs to drive the red, green and blue LEDs.
+--**********************************************************************
 
 library IEEE, XESS;
 use IEEE.std_logic_1164.all;
@@ -94,7 +100,7 @@ architecture arch of RgbLed is
 begin
 
   -- Three PWMs drive the red, green and blue LED outputs.
-  -- The high duty cycle % of an LED is the intensity / 2**intensity'length.
+  -- The duty cycle % of an LED is the intensity / 2**intensity'length.
   -- An LED is on when its driver is pulled low which happens when its PWM output is high.
   -- An LED is off when its driver is tristated which happens when its PWM output is low.
   
@@ -126,6 +132,13 @@ end architecture;
 
 
 
+--**********************************************************************
+-- A Wishbone-compliant driver for an RGB LED.
+-- It contains a single 32-bit register at address 0 whose lower three 
+-- bytes each control the duty cycle of a PWM attached to one of the red,
+-- green or blue LEDs.
+--**********************************************************************
+
 library IEEE, XESS;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
@@ -149,7 +162,7 @@ entity WbRgbLed is
     wb_we_i   : in  std_logic;          -- Write-enable.
     wb_cyc_i  : in  std_logic;          -- Valid cycle indicator.
     wb_stb_i  : in  std_logic;          -- Chip-enable.
-    wb_ack_o  : out std_logic;  -- Acknowledge upon completion of operation.
+    wb_ack_o  : out std_logic;          -- Acknowledge upon completion of operation.
     wb_inta_o : out std_logic;          -- Interrupt from RGB LED driver.
     id        : out std_logic_vector;   -- Identifier for RGB LED driver.
     -- RGB LED drivers.
@@ -164,31 +177,33 @@ architecture arch of WbRgbLed is
   signal red_r, grn_r, blu_r : std_logic_vector(7 downto 0);  -- Red, green and blue intensity registers.
 begin
 
-  id        <= VENDOR_ID_G & PRODUCT_ID_G;  -- Output the vendor and product IDs so the ZPUino can identify it.
-  wb_inta_o <= NO;                      -- No interrupts come from this module.
+  id <= VENDOR_ID_G & PRODUCT_ID_G;  -- Output the vendor and product IDs so the ZPUino can identify it.
+  
+  wb_inta_o <= NO;  -- No interrupts come from this module.
 
   wbActive_s <= wb_cyc_i and wb_stb_i;  -- True when this device is read/written over Wishbone bus.
   wb_ack_o   <= wbActive_s;  -- Immediately acknowledge any read or write operation.
 
+  -- This process controls the reading/writing of the RGB intensity register.
   process(wb_clk_i)
   begin
     if rising_edge(wb_clk_i) then
-      wb_dat_o <= (others => '0');
-      if wbActive_s = YES then          -- This device is being accessed.
-        if wb_we_i = YES then
+      wb_dat_o <= (others => '0');  -- The default data output for this device is all zeroes.
+      if wbActive_s = YES then  -- True when this device is being accessed over the Wishbone bus.
+        if wb_we_i = YES then   -- True when this device is being written.
           case TO_INTEGER(unsigned(wb_adr_i(9 downto 2))) is
-            when 0 =>                   -- Write R, G, B intensities.
+            when 0 =>  -- Writing address 0 sets the R, G, B intensities.
               red_r <= wb_dat_i(7 downto 0);
               grn_r <= wb_dat_i(15 downto 8);
               blu_r <= wb_dat_i(23 downto 16);
-            when others =>
+            when others =>  -- Writing other addresses does nothing.
               null;
           end case;
-        else
+        else  -- This device is being read.
           case TO_INTEGER(unsigned(wb_adr_i(9 downto 2))) is
-            when 0 =>                   -- Read R, G, B intensities.
+            when 0 =>  -- Reading address 0 gets the R, G, B intensities.
               wb_dat_o(23 downto 0) <= blu_r & grn_r & red_r;
-            when others =>
+            when others =>  -- Reading other addresses does nothing.
               null;
           end case;
         end if;
@@ -196,7 +211,8 @@ begin
     end if;
   end process;
 
-  -- Apply the red, green and blue intensities to the RGB LED driver.
+  -- Apply the red, green and blue intensities from the register to the RGB LED driver module
+  -- described previously.
   uRgbLed : RgbLed
     port map(
       clk_i    => wb_clk_i,
@@ -209,7 +225,3 @@ begin
       );
 
 end architecture;
-
-
-
-
