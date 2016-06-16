fce5c5a4b34ea4306bbfe76eb60dd1c13a134b61
Dave Vandenbout <devb@xess.com>
2016-06-16 15:57:56 -0400

    SPI data word is now set by width of input data vector. * SPI timing parameters are now settable as generics. * Fixed error where mosi_o was not setup before initial sck_o pulse.

--- Spi.vhd
+++ Spi.vhd
@@ -25,17 +25,20 @@ package SpiPckg is
 
   component SpiMaster is
     generic (
-      FREQ_G     : real      := 100.0;  -- Main clock frequency (MHz).
-      SPI_FREQ_G : real      := 25.0;   -- SPI clock frequency (MHz).
-      CPOL_G     : std_logic := LO;  -- SCK polarity (0=normally low, 1=normally high).
-      CPHA_G     : std_logic := HI  -- SCK phase (0=sample on leading edge, 1=sample on trailing edge).
+      FREQ_G              : real      := 100.0;  -- Main clock frequency (MHz).
+      SPI_FREQ_G          : real      := 25.0;  -- SPI clock frequency (MHz).
+      DISABLE_TIME_G      : real      := 40.0;  -- Disable time between transmissions (ns).
+      ENABLE_SETUP_TIME_G : real      := 40.0;  -- SSEL assertion until 1st clock (ns).
+      ENABLE_HOLD_TIME_G  : real      := 40.0;  -- Last clock until SSEL release (ns).
+      CPOL_G              : std_logic := LO;  -- SCK polarity (0=normally low, 1=normally high).
+      CPHA_G              : std_logic := HI  -- SCK phase (0=sample on leading edge, 1=sample on trailing edge).
       );
     port (
       rst_i   : in  std_logic := NO;    -- Active-high reset input.
       clk_i   : in  std_logic;          -- Master clock input.
       addr_i  : in  std_logic_vector(1 downto 0);  -- Register address from PC.
-      data_i  : in  std_logic_vector(7 downto 0);  -- Data from PC.
-      data_o  : out std_logic_vector(7 downto 0);  -- Data to PC.
+      data_i  : in  std_logic_vector;   -- Data from PC.
+      data_o  : out std_logic_vector;   -- Data to PC.
       wr_i    : in  std_logic := LO;    -- Write control from PC.
       rd_i    : in  std_logic := LO;    -- Read control from PC.
       begun_o : out std_logic;          -- True when R/W operation has begun.
@@ -56,22 +59,23 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.math_real.all;
 use XESS.CommonPckg.all;
---library unisim;
---use unisim.vcomponents.all;
 
 entity SpiMaster is
   generic (
-    FREQ_G     : real      := 100.0;    -- Main clock frequency (MHz).
-    SPI_FREQ_G : real      := 25.0;     -- SPI clock frequency (MHz).
-    CPOL_G     : std_logic := LO;  -- SCK polarity (0=normally low, 1=normally high).
-    CPHA_G     : std_logic := HI  -- SCK phase (0=sample on leading edge, 1=sample on trailing edge).
+    FREQ_G              : real      := 100.0;    -- Main clock frequency (MHz).
+    SPI_FREQ_G          : real      := 25.0;  -- SPI clock frequency (MHz).
+    DISABLE_TIME_G      : real      := 40.0;  -- Disable time between transmissions.
+    ENABLE_SETUP_TIME_G : real      := 40.0;  -- SSEL assertion until 1st clock.
+    ENABLE_HOLD_TIME_G  : real      := 40.0;  -- Last clock until SSEL release.
+    CPOL_G              : std_logic := LO;  -- SCK polarity (0=normally low, 1=normally high).
+    CPHA_G              : std_logic := HI  -- SCK phase (0=sample on leading edge, 1=sample on trailing edge).
     );
   port (
     rst_i   : in  std_logic := NO;      -- Active-high reset input.
     clk_i   : in  std_logic;            -- Master clock input.
     addr_i  : in  std_logic_vector(1 downto 0);  -- Register address from PC.
-    data_i  : in  std_logic_vector(7 downto 0);  -- Data from PC.
-    data_o  : out std_logic_vector(7 downto 0);  -- Data to PC.
+    data_i  : in  std_logic_vector;     -- Data from PC.
+    data_o  : out std_logic_vector;     -- Data to PC.
     wr_i    : in  std_logic := LO;      -- Write control from PC.
     rd_i    : in  std_logic := LO;      -- Read control from PC.
     begun_o : out std_logic;            -- True when R/W operation has begun.
@@ -85,13 +89,13 @@ end entity;
 
 architecture arch of SpiMaster is
   subtype regAddr_t is std_logic_vector(addr_i'range);
-  constant RESET_ADDR            : regAddr_t := "00";
-  constant SINGLE_XFER_ADDR      : regAddr_t := "01";
-  constant MULTI_XFER_ADDR       : regAddr_t := "10";
-  constant CLKS_PER_SCLK_PHASE_C : natural   := integer(round(0.5 * FREQ_G / SPI_FREQ_G));
-  constant DISABLE_TIME_C        : natural   := 10;
-  constant ENABLE_SETUP_TIME_C   : natural   := 5;
-  constant ENABLE_HOLD_TIME_C    : natural   := 7;
+  constant RESET_ADDR_C          : regAddr_t := "00";  -- Access this address to reset SPI.
+  constant SINGLE_XFER_ADDR_C    : regAddr_t := "01";  -- Address for single transfer.
+  constant MULTI_XFER_ADDR_C     : regAddr_t := "10";  -- Address for multiple transfers.
+  constant SCLK_PHASE_CYCLES_C   : natural   := integer(round(0.5 * FREQ_G / SPI_FREQ_G));
+  constant DISABLE_CYCLES_C      : natural   := integer(round(DISABLE_TIME_G * FREQ_G / 1000.0));
+  constant ENABLE_SETUP_CYCLES_C : natural   := integer(round(ENABLE_SETUP_TIME_G * FREQ_G / 1000.0));
+  constant ENABLE_HOLD_CYCLES_C  : natural   := integer(round(ENABLE_HOLD_TIME_G * FREQ_G / 1000.0));
 
   signal sck_r     : std_logic := CPOL_G;
   signal sr_r      : std_logic_vector(data_i'range);
@@ -100,14 +104,14 @@ begin
 
   process (clk_i)
     variable rst_v              : boolean := false;
-    variable sclkPhaseTimer_v   : natural range 0 to CLKS_PER_SCLK_PHASE_C;
-    variable sselTimer_v        : natural range 0 to DISABLE_TIME_C;
+    variable sclkPhaseTimer_v   : natural range 0 to SCLK_PHASE_CYCLES_C-1;
+    variable sselTimer_v        : natural range 0 to DISABLE_CYCLES_C-1;
     variable disableAfterXfer_v : boolean;
     variable bitCntr_v          : natural range 0 to sr_r'length;
   begin
     if rising_edge(clk_i) then
 
-      -- These acknowledgement signals are off by default.
+      -- Set acknowledgement signal default levels.
       done_o  <= NO;
       begun_o <= NO;
 
@@ -129,23 +133,23 @@ begin
       elsif enabled_r and bitCntr_v = 0 and disableAfterXfer_v then
         enabled_r          <= false;
         disableAfterXfer_v := false;
-        sselTimer_v        := DISABLE_TIME_C - 1;
+        sselTimer_v        := DISABLE_CYCLES_C - 1;
 
       -- If bit xfer is not active, then look for R/W operations from the host.
       elsif bitCntr_v = 0 then
         if wr_i = YES or rd_i = YES then
-          if addr_i = RESET_ADDR then
+          if addr_i = RESET_ADDR_C then
             rst_v   := true;
             begun_o <= YES;
             done_o  <= YES;
           else
-            sclkPhaseTimer_v   := CLKS_PER_SCLK_PHASE_C - 1;
+            sclkPhaseTimer_v   := SCLK_PHASE_CYCLES_C - 1;
             sr_r               <= data_i;
             begun_o            <= YES;
             enabled_r          <= true;
-            sselTimer_v        := ENABLE_SETUP_TIME_C;
+            sselTimer_v        := ENABLE_SETUP_CYCLES_C - 1;
             disableAfterXfer_v := false;
-            if addr_i = SINGLE_XFER_ADDR then
+            if addr_i = SINGLE_XFER_ADDR_C then
               disableAfterXfer_v := true;
             end if;
             bitCntr_v := sr_r'length;
@@ -159,20 +163,18 @@ begin
       -- Generate SPI clock transition and handle bit xfer.
       else
         if sck_r = (CPOL_G xor CPHA_G) then
-          -- Sample data from the slave on the leading edge of SCK when CPHA_G=0
-          -- and the trailing edge when CPHA_G=1.
+          -- Output new data to the slave and sample data from the slave on the
+          -- leading edge of SCK when CPHA_G=0 and the trailing edge when CPHA_G=1.
           sr_r <= sr_r(sr_r'high-1 downto 0) & miso_i;
-        else
-          -- Propagate data to the slave on the opposite edge of SCK.
-          mosi_o <= sr_r(sr_r'high);
         end if;
+
         -- Decrement bit counter on the trailing edge of SCK.
         if sck_r = not CPOL_G then
           if bitCntr_v /= 0 then
             if bitCntr_v = 1 then
               done_o <= YES;
               if disableAfterXfer_v = true then
-                sselTimer_v := ENABLE_HOLD_TIME_C - 1;
+                sselTimer_v := ENABLE_HOLD_CYCLES_C - 1;
               end if;
             end if;
             bitCntr_v := bitCntr_v - 1;
@@ -181,12 +183,13 @@ begin
 
         -- Transition the SPI clock and set the phase interval.
         sck_r            <= not sck_r;
-        sclkPhaseTimer_v := CLKS_PER_SCLK_PHASE_C - 1;
+        sclkPhaseTimer_v := SCLK_PHASE_CYCLES_C - 1;
       end if;
       
     end if;
   end process;
 
+  mosi_o <= sr_r(sr_r'high);
   ssel_o <= LO when enabled_r = true else HI;
   sck_o  <= sck_r;
   data_o <= sr_r;
